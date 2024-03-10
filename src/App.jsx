5832faceec7619c80bb1cb5b3266db6fd11ca595
Eduard <edo@gmail.com>
2024-03-11 00:41:41 +0400

    Fixed: imagies,icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import logo from "./media/logo/logo.svg.png";
+import menu_svg from "./media/icons/menu.svg.png";
+import arrow_icon from "./media/icons/arrow-bottom.svg.png";
+import main_img from "./media/images/div.png";
+import sponsor1 from "./media/sponsors_logo/logo-ycombinator.svg.png";
+import sponsor2 from "./media/sponsors_logo/logo-coinbase.svg.png";
+import sponsor3 from "./media/sponsors_logo/logo-a16z.png.png";
+import sponsor4 from "./media/sponsors_logo/logo-blockchange.svg.png";
 
 class App extends React.Component{
   render(){
@@ -8,7 +15,7 @@ class App extends React.Component{
       <>
         <header>
             <div id="logo_container">
-                <img src="media/logo/logo.svg.png" alt="logo" id="logo_img"/>
+                <img src = {logo} alt="logo" id="logo_img"/>
             </div>
             <div id="sections_container">
                 <div className="section_wrapper">
@@ -27,7 +34,7 @@ class App extends React.Component{
             </div>
             <div id="menu_container">
                 <div id="menu_p">Menu</div>
-                <img src="media/icons/menu.svg.png" alt="menu_bar" id="menu_bar"/>
+                <img src={menu_svg} alt="menu_bar" id="menu_bar"/>
             </div>
         </header>
         <main>
@@ -38,28 +45,28 @@ class App extends React.Component{
                     web3 world<br/>
                 </div>
                 <div id="rotated_content_container">
-                    <img src="media/icons/arrow-bottom.svg.png" alt="arrow_up" id="content_arrow_img"/>
+                    <img src={arrow_icon} alt="arrow_up" id="content_arrow_img"/>
                     <p id="rotated_content_p">We help companies design their products to be ready for web3 world</p>
                 </div>
             </div>
             <div id="main_img_container">
-                <img src="media/images/div.png" alt="phone" id="main_img"/>
+                <img src={main_img} alt="phone" id="main_img"/>
             </div>
         </main>
         <footer>
             <p id="backed_by">Our clients are backed by:</p>
             <div id="companies_container">
                 <div className="companie_logo_container">
-                    <img src="media/sponsors_logo/logo-ycombinator.svg.png" alt="companie" className="companie_logo"/>
+                    <img src={sponsor1} alt="companie" className="companie_logo"/>
                 </div>
                 <div className="companie_logo_container">
-                    <img src="media/sponsors_logo/logo-coinbase.svg.png" alt="companie" className="companie_logo"/>
+                    <img src={sponsor2} alt="companie" className="companie_logo"/>
                 </div>
                 <div className="companie_logo_container">
-                    <img src="media/sponsors_logo/logo-a16z.png.png" alt="companie" className="companie_logo"/>
+                    <img src={sponsor3} alt="companie" className="companie_logo"/>
                 </div>
                 <div className="companie_logo_container">
-                    <img src="media/sponsors_logo/logo-blockchange.svg.png" alt="companie" className="companie_logo"/>
+                    <img src={sponsor4} alt="companie" className="companie_logo"/>
                 </div>
             </div>
         </footer>
